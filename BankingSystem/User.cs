6f1f8be90f88f5cfdef8b24da30291da1ce2ccba
AlisherB <alisher_b_95@mail.ru>
2017-08-30 17:14:36 +0600

    Edited class User

--- BankingSystem/User.cs
+++ BankingSystem/User.cs
@@ -1,9 +1,18 @@
-﻿namespace BankingSystem
+﻿using System.Collections.Generic;
+
+namespace BankingSystem
 {
     public class User
     {
-        public long Id { get; set; }
-
+        public int Id { get; set; }
+        public string IIN { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string IdCardNumber { get; set; }
+        public string TelephoneNumber { get; set; }
+        public List<Account> Accounts { get; set; }
+        public List<Card> Cards { get; set; }
         public string Email { get; set; }
     }
 }
