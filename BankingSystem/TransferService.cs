55a9fe14098828450f398946c7e73f32dfe0a971
Aibek Shulembekov <aibek1805@gmail.com>
2017-08-30 17:50:59 +0600

    Update made Transfer Service

--- BankingSystem/TransferService.cs
+++ BankingSystem/TransferService.cs
@@ -1,11 +1,23 @@
-﻿namespace BankingSystem
+﻿
+using System;
+
+namespace BankingSystem
 {
     public class TransferService : ITransferService
     {
         public long Id { get; set; }
-        public void Transfer(Account account, Account transferaccount, double Money)
+        public bool Transfer(Account account, Account transferaccount, double Money)
         {
-            if(account.MoneyOnAccount
+            if (account.MoneyOnAccount < Money)
+            {
+                return false;
+            }
+            else
+            {
+                transferaccount.MoneyOnAccount += Money;
+                account.MoneyOnAccount -= Money;
+                return true;
+            }
         }
     }
 }
