8a4a1898627008cc39467fe241e09750bfb236d7
aiba1805 <aibek1805@gmail.com>
2017-09-01 19:24:00 +0600

    Added Tlegram Bot on github

---
+++ TelegramBot/Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "http://bankingsystemtelegrambot.azurewebsites.net/";
+
+        public static string Name { get; set; } = "banking_system_bot";
+
+        public static string Key { get; set; } = "422108608:AAEKs1HE_ZiXfl3okH3OctIACLCAX6q1eOY";
+
+    }
+}
